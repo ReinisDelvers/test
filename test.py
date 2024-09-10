52a3ac611b4ac27c1d1c81641f809b08cbb3d15b
ReinisDelvers <reinis.delvers@gmail.com>
2024-09-10 12:30:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,34 @@
-print("asfd")
+class Cilveks:
+    def __init__(self, vards, dzimums, vecums):
+        self.vards = vards
+        self.dzimums = dzimums
+        self.vecums = vecums
+
+    def dzimsanasdiena(self):
+        self.vecums += 1
+    
+    def pastastitparsevi(self):
+        if self.dzimums == "V":
+            dzimumanosaukums = "Virietis"
+        elif self.dzimums == "S":
+            dzimumanosaukums = "Sieviete"
+        else:
+            dzimumanosaukums = self.dzimums
+
+        print("Sveiki mani sauc {}. Man ir {} gadi. Es esmu {}".format(self.vards, self.vecums, dzimumanosaukums))
+    
+    def dzimummaina(self):
+        if self.dzimums == "V":
+            self.dzimums = "S"
+        elif self.dzimums == "S":
+            self.dzimums = "V"
+
+    def vardamaina(self, jaunsvards):
+        self.vards = jaunsvards
+
+persona = Cilveks("Reinis", "V", 18)
+persona.pastastitparsevi()
+persona.dzimsanasdiena()
+persona.dzimummaina()
+persona.vardamaina("Aleksis")
+persona.pastastitparsevi()
