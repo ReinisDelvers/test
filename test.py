3a4617345ddd70ec36375f82b8c3782cbd6403a8
ReinisDelvers <reinis.delvers@gmail.com>
2024-09-12 16:45:56 +0300

    .

--- test.py
+++ test.py
@@ -20,46 +20,46 @@ class Person:
             self.sex = "S"
         elif self.sex == "S":
             self.sex = "V"
-        persona.tellmeaboutmyself()
+        # persona.tellmeaboutmyself()
 
 
     def birthday(self):
         self.age += 1
-        persona.tellmeaboutmyself()
+        # persona.tellmeaboutmyself()
 
     
     def namechange(self, newname):
         self.name = newname
-        persona.tellmeaboutmyself()
+        # persona.tellmeaboutmyself()
 
 class Women(Person):
     
     def __init__(self, name, haircolor, age = 0):
         super().__init__(name,"S", age)
         self.haircolor = haircolor
-        self.tellmeaboutmyself()
+        # self.tellmeaboutmyself()
         
-    def tellmeaboutmyself(self):
-        super().tellmeaboutmyself()
-        print(f"Mana matu krāsa ir {self.haircolor}")
+    # def tellmeaboutmyself(self):
+    #     super().tellmeaboutmyself()
+    #     print(f"Mana matu krāsa ir {self.haircolor}")
 
 class Men(Person):
     
     def __init__(self, name, hobby, age = 0):
         super().__init__(name,"S", age)
         self.hobby = hobby
-        self.tellmeaboutmyself()
+        # self.tellmeaboutmyself()
         
-    def tellmeaboutmyself(self):
-        super().tellmeaboutmyself()
-        print(f"Mans hobijs ir {self.hobby}")
+    # def tellmeaboutmyself(self):
+    #     super().tellmeaboutmyself()
+    #     print(f"Mans hobijs ir {self.hobby}")
 
 
 persona = Person("Aleksis", "V", 18)
-persona.tellmeaboutmyself()
-persona.birthday()
-persona.sexchange()
-persona.namechange("Alex")
+# persona.tellmeaboutmyself()
+# persona.birthday()
+# persona.sexchange()
+# persona.namechange("Alex")
 
-persona = Women("Anna", "blonda", 23)
-persona = Men("Pēteris", "sērfošana", 35)
+# persona = Women("Anna", "blonda", 23)
+# persona = Men("Pēteris", "sērfošana", 35)
