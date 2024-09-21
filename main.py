2afbd0974550fa9f5ad9f6286e7a7b0e021a1273
ReinisDelvers <141557986+ReinisDelvers@users.noreply.github.com>
2024-09-21 12:47:18 +0300

    .

--- main.py
+++ main.py
@@ -24,69 +24,139 @@ sex_label = ttk.Label(frame, text='Sex')
 sex_label.grid(column=0, row=1, sticky='W', **options)
 
 age_label = ttk.Label(frame, text='Age')
-age_label.grid(column=0, row=2, sticky='W', **options)
+age_label.grid(column=0, row=3, sticky='W', **options)
 
-name = tk.StringVar()
+name = tk.StringVar(value="")
 name_entry = ttk.Entry(frame, textvariable=name)
 name_entry.grid(column=1, row=0, **options)
 name_entry.focus()
 
-sex = tk.StringVar()
-sex_entry = ttk.Entry(frame, textvariable=sex)
-sex_entry.grid(column=1, row=1, **options)
-sex_entry.focus()
+sex = tk.StringVar(value="None")
+R1 = tk.Radiobutton(frame, text="Vīrietis", variable=sex, value="Vīrietis")
+R1.grid(column=1, row=1, **options)
 
-age = tk.IntVar()
+
+R2 = tk.Radiobutton(frame, text="Sieviete", variable=sex, value="Sieviete")
+R2.grid(column=1, row=2, **options)
+
+
+age = tk.StringVar(value="")
 age_entry = ttk.Entry(frame, textvariable=age)
-age_entry.grid(column=1, row=2, **options)
+age_entry.grid(column=1, row=3, **options)
 age_entry.focus()
+
+newname = tk.StringVar(value="")
+newname_entry = ttk.Entry(frame, textvariable=newname)
+newname_entry.grid(column=3, row=0, **options)
+newname_entry.focus()
+
 
 def razot_button_clicked():
     name1 = name.get()
     sex1 = sex.get()
     age1 = age.get()
+    Error=0
+    try:
+        int(age1)
+    except ValueError:
+        Error=1
+
+    if sex1 == "None" or name1 == "":
+        showinfo("Error", "Please fill all fields.")
+        return
+    elif Error == 1:
+        showinfo("Error", "Please fill field age with a number.")
+        return
     allpeople.append(Person(name1, sex1, age1))
     print(name1, sex1, age1)
     result_label.config(text=allpeople[-1].info())
-    list(allpeople)
+    list()
 
-button = ttk.Button(frame, text='Produce person')
-button.grid(column=2, row=0, sticky='W', **options)
-button.configure(command=razot_button_clicked)
+def sexchange():
+    if sex == "Vīriets":
+        sex = "Sieviete"
+    elif sex == "Sieviete":
+        sex = "Vīriets"
+    mainit_button_clicked()
+
+
+
+def birthday():
+    age2 = age.get()
+    age2 += 1
+    mainit_button_clicked(age2)
+
+
+
+def namechange(newname):
+    name = newname
+    mainit_button_clicked()
+
+def mainit_button_clicked(age2):
+    name3 = name
+    sex3 = sex
+    age3 = age2
+    Error=0
+    try:
+        int(age3)
+    except ValueError:
+        Error=1
+
+    if sex3 == "None" or name3 == "":
+        showinfo("Error", "Please fill all fields.")
+        return
+    elif Error == 1:
+        showinfo("Error", "Please fill field age with a number.")
+        return
+    allpeople.append(Person(name3, sex3, age3))
+    print(name3, sex3, age3)
+    result_label.config(text=allpeople[-1].info())
+    list()
+
+production_button = ttk.Button(frame, text='Produce person')
+production_button.grid(column=2, row=0, sticky='W', **options)
+production_button.configure(command=razot_button_clicked)
+
+sexchange_button = ttk.Button(frame, text='Sex change')
+sexchange_button.grid(column=4, row=1, sticky='W', **options)
+sexchange_button.configure(command=sexchange)
+
+namechange_button = ttk.Button(frame, text='Name change')
+namechange_button.grid(column=4, row=0, sticky='W', **options)
+namechange_button.configure(command=namechange)
+
+birthday_button = ttk.Button(frame, text='Birthday')
+birthday_button.grid(column=4, row=3, sticky='W', **options)
+birthday_button.configure(command=birthday)
 
 result_label = ttk.Label(frame)
-result_label.grid(row=3, columnspan=3, **options)
+result_label.grid(row=4, columnspan=3, **options)
 
 
-print(f"0 + {allpeople}")
+    
+# def list():
+#     tuple(allpeople)
+#     var = tk.Variable(value=allpeople)
 
-def list(allpeople):
-    print(f"00 + {allpeople}")
-    tuple(allpeople)
-    var = tk.Variable(value=allpeople)
+#     listbox = tk.Listbox(
+#         root,
+#         listvariable=var,
+#         height=6,
+#         selectmode=tk.EXTENDED
+#     )
 
-    listbox = tk.Listbox(
-        root,
-        listvariable=var,
-        height=6,
-        selectmode=tk.EXTENDED
-    )
+#     listbox.grid(column=0, row=4)
 
-    listbox.grid(column=0, row=4)
-
-    def items_selected(event):
-        # get all selected indices
-        selected_indices = listbox.curselection()
-        # get selected items
-        selected_langs = ",".join([listbox.get(i) for i in selected_indices])
-        msg = f'You selected: {selected_langs}'
-        showinfo(title='Information', message=msg)
+#     def items_selected(event):
+#         # get all selected indices
+#         selected_indices = listbox.curselection()
+#         # get selected items
+#         selected_langs = ",".join([listbox.get(i) for i in selected_indices])
+#         msg = f'You selected: {selected_langs}'
+#         showinfo(title='Information', message=msg)
 
 
-    listbox.bind('<<ListboxSelect>>', items_selected)
-
-
-
+#     listbox.bind('<<ListboxSelect>>', items_selected)
 
 label2 = ttk.Label(frame)
 label2.grid(row=3, columnspan=3, **options)
